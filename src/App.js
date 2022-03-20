86ef8d294bcea473f9c57265bc7aa7a416d660aa
Omar452 <56162863+Omar452@users.noreply.github.com>
2022-03-20 17:23:06 +0000

    Create city and cities states

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import { useState } from 'react';
+import SearchCity from './components/SearchCity';
 
 function App() {
+  const [city, setCity] = useState(null);
+  const [cities, setCities] = useState(null);
+
   return (
     <div className="App">
-    
+      <SearchCity setCities={setCities} setCity={setCity}/>
     </div>
   );
 }
